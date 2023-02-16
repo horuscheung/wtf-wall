39c3835e915faf640ec36439e14d3c4af9aa457e
Horus <horuscheung@iasia.com.hk>
2023-02-16 14:05:41 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 4000;
+let dailyWtf = 0;
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  io.emit("dailyWtf", dailyWtf);
+  socket.on("wtf", (msg) => {
+    dailyWtf++;
+    io.emit("wtf", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
